3634bfa3f9a33c9f6e35ad7760a04c687784b174
Amedir <ademirmsfilho@fac.pe.senac.br>
2022-03-08 12:34:55 -0300

    Atualiza API Usuario

--- Usuario/src/main/java/com/example/demo/entities/Usuario.java
+++ Usuario/src/main/java/com/example/demo/entities/Usuario.java
@@ -8,53 +8,43 @@ import javax.persistence.Id;
 @Entity
 public class Usuario {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
- private Long id;
- private String nome;
- private String email;
- private String contato;
- //private Endereco endereco;
- 
- public Usuario(Long id, String nome, String email, String contato) {
-		super();
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String email;
+	private String contato;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getContato() {
+		return contato;
+	}
+
+	public void setContato(String contato) {
 		this.contato = contato;
 	}
- 
- public Usuario() {
-	super();
-}
 
-
-
-public Long getId() {
-	return id;
-}
-
-public void setId(Long id) {
-	this.id = id;
-}
-public String getNome() {
-	return nome;
-}
-public void setNome(String nome) {
-	this.nome = nome;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public String getContato() {
-	return contato;
-}
-public void setContato(String contato) {
-	this.contato = contato;
-}
- 
- 
 }
--- Usuario/src/main/resources/import.sql
+++ Usuario/src/main/resources/import.sql
@@ -1,5 +1,3 @@
-INSERT INTO usuario(contato,email,nome) values ('(81)98753-7234','jhoglapsql@gmail.com','Jhonny');
-INSERT INTO usuario(contato,email,nome) values ('(81)96294-7234','enricoaccenture@gmail.com','Enrico');
-INSERT INTO usuario(contato,email,nome) values ('(22)98753-6328','amedirfoguete@gmail.com','Ademir');
-INSERT INTO usuario(contato,email,nome) values ('(81)97234-0129','Alissonaruto@gmail.com','Alisson');
-INSERT INTO usuario(contato,email,nome) values ('(82)95012-9127','gihcellysaiugente@gmail.com','Gihcelly');
+INSERT INTO usuario(contato,email,nome) values ('(81)98753-7234','j.glaucio.melo@accenture.com','Jhonny');
+INSERT INTO usuario(contato,email,nome) values ('(81)96294-7234','m.enrico.silva@accenture.com','Enrico');
+INSERT INTO usuario(contato,email,nome) values ('(22)98753-6328','a.monteiro.silva@accenture.com','Ademir');
